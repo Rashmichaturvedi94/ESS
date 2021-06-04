3d91dbf2d2adef9253337168d986eeced03a7d7e
Manpreet Singh <mann.singh579@gmail.com>
2021-06-04 18:56:15 +0200

    mock data

--- frontend/src/containers/SearchCourse/SearchCourse.tsx
+++ frontend/src/containers/SearchCourse/SearchCourse.tsx
@@ -2,9 +2,11 @@ import React, { FC } from 'react';
 import { GridList, Typography, GridListTile } from '@material-ui/core';
 import { SearchCourseProps } from './SearchCourse.interface';
 import { useCourses } from '../../api';
+import { course as dummy } from '../../mocks/fixtures';
 
 export const SearchCourse: FC<SearchCourseProps> = () => {
   const courses = useCourses();
+  console.log(dummy.id);
   return (
     <GridList>
       {courses.data?.map((course) => (
---
+++ frontend/src/mocks/fixtures.ts
@@ -0,0 +1,8 @@
+export const course = {
+  name: '',
+  descroption: '',
+  id: 1,
+  price: 12.50,
+  created_by: 1,
+  
+};
