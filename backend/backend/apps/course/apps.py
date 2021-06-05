69b0634132b4670f742a4e36f057ee1da27dfbd0
Manpreet Singh <mann.singh579@gmail.com>
2021-06-05 14:29:22 +0200

    models error fix

--- backend/backend/apps/course/apps.py
+++ backend/backend/apps/course/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'course'
+    name = 'backend.apps.course'
     verbose_name = 'Course App'
