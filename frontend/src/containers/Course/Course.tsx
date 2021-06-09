71534bb145ca13aaa870a75e9977e04d40362f88
sukhi0077 <sukhi0077@gmail.com>
2021-06-10 01:23:31 +0200

    navigation fix

--- frontend/src/containers/Course/Course.tsx
+++ frontend/src/containers/Course/Course.tsx
@@ -1,28 +1,41 @@
-import React, { FC } from 'react';
-import { Box } from '@material-ui/core';
-import { Course as CourseComponent } from './Course.styles';
-import { CourseProps } from './Course.interface';
-import { CourseForm } from '../../components/CourseForm';
-import { usePostCourse } from '../../api';
+import React, { FC } from "react";
+import { Box } from "@material-ui/core";
+import { useHistory, generatePath } from "react-router-dom";
+import { appPaths } from "../../const/paths";
+import { Course as CourseComponent } from "./Course.styles";
+import { CourseProps } from "./Course.interface";
+import { CourseForm } from "../../components/CourseForm";
+import { usePostCourse } from "../../api";
 
 export const Course: FC<CourseProps> = () => {
   const { mutate } = usePostCourse();
-  
+  const history = useHistory();
   return (
-  <CourseComponent>
-  <Box flex={1} display="flex" alignItems="center" justifyContent="center">
-    <Box width="30%" >
-    <CourseForm
-       onSubmit={(data) => {
-          mutate({ data });
-       }
-      }
-    />
-     
+    <CourseComponent>
+      <Box flex={1} display="flex" alignItems="center" justifyContent="center">
+        <Box width="30%">
+          <CourseForm
+            onSubmit={(data) => {
+              mutate(
+                { data },
+                {
+                  onSuccess: (d) => {
+                    history.push(
+                      generatePath(appPaths.createCourseContent, {
+                        courseId: d.id,
+                      })
+                    );
+                    console.log(d);
+                  },
+                }
+              );
+            }}
+          />
+        </Box>
+        <Box display="flex" flexDirection="column" width={320} gridRowGap={20}>
+          <div />
+        </Box>
       </Box>
-  <Box display="flex" flexDirection="column" width={320} gridRowGap={20}>
-  <div/>
-  </Box>
-</Box></CourseComponent>
-);
+    </CourseComponent>
+  );
 };
--- frontend/src/containers/MyCourses/MyCourses.tsx
+++ frontend/src/containers/MyCourses/MyCourses.tsx
@@ -1,28 +1,44 @@
-import React, { FC } from 'react';
-import { Box, GridList, Typography, GridListTile } from '@material-ui/core';
-// import { SubscribedTitle } from './MyCourses.styles';
-// import { MyCourses as MyCoursesComponent, SubscribedTitle } from './MyCourses.styles';
-import { MyCoursesProps } from './MyCourses.interface';
-import { useSubscriptions } from '../../api';
-import { Course } from '../../api/api.interface';
+import React, { FC } from "react";
+import { Box, GridList, Typography, GridListTile } from "@material-ui/core";
+import { useHistory, generatePath  } from 'react-router-dom';
+import { appPaths } from '../../const/paths';
+import { MyCoursesProps } from "./MyCourses.interface";
+import { useSubscriptions } from "../../api";
+import { Course } from "../../api/api.interface";
 
 export const MyCourses: FC<MyCoursesProps> = () => {
   const subscriptions = useSubscriptions();
-  const getImage = (course: Course) => typeof course.img === 'string' ? course.img : '';
+  const history = useHistory();
+
+  const getImage = (course: Course) =>
+    typeof course.img === "string" ? course.img : "";
   return (
-    <Box style={{ marginLeft: 20, marginRight: 20}}>
-      <Typography variant="h4" component="h2" align='left'>
+    <Box style={{ marginLeft: 20, marginRight: 20 }}>
+      <Typography variant="h4" component="h2" align="left">
         Subscribed
       </Typography>
       <GridList>
         {subscriptions.data?.map((subscription) => (
-          <GridListTile key={subscription.course.id} style={{ height: 258, width: 224, border: '2px solid black', margin: 8}}>
-            <img src={getImage(subscription.course)} style={{ height: 196, width:220, overflow: 'hidden' }} alt={subscription.course.title}/>
+          <GridListTile
+            key={subscription.course.id}
+            onClick={() => history.push(generatePath(appPaths.courseContent, { courseId: subscription.course.id }))}
+            style={{
+              height: 258,
+              width: 224,
+              border: "2px solid black",
+              margin: 8,
+            }}
+          >
+            <img
+              src={getImage(subscription.course)}
+              style={{ height: 196, width: 220, overflow: "hidden" }}
+              alt={subscription.course.title}
+            />
             <Typography>{subscription.course.title}</Typography>
             <Typography>{subscription.course.description}</Typography>
           </GridListTile>
         ))}
       </GridList>
-  </Box>
+    </Box>
   );
 };
