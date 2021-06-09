9756704e1217d91670bec0d78a1807af01e25b35
sukhi0077 <sukhi0077@gmail.com>
2021-06-09 23:52:49 +0200

    Update Subscription.tsx

--- frontend/src/containers/Subscription/Subscription.tsx
+++ frontend/src/containers/Subscription/Subscription.tsx
@@ -5,19 +5,20 @@ import { SubContainer } from "./Subscription.styles";
 import { useCourse, usePostSubscription } from "../../api";
 import { SubscriptionForm } from "../../components/SubscriptionForm";
 import { getUserIdFromLocalStorage } from "../../api/api.helpers";
-
-import { CourseParams } from "../../api/api.interface";
+import { Course, CourseParams } from "../../api/api.interface";
 
 export const Subscription: FC = () => {
   const { courseId } = useParams<CourseParams>();
   const course = useCourse({ courseId });
   const { mutate } = usePostSubscription();
+  const getImage = (courseObj?: Course) => typeof courseObj?.img === 'string' ? courseObj?.img : '';
+
   return (
     <SubContainer>
       <Box style={{ marginLeft: 50, marginTop: 50 }}>
         <Box style={{ marginLeft: 40, marginRight: 110 }}>
           <img
-            src="https://blog.wildix.com/wp-content/uploads/2020/06/react-logo.jpg"
+            src={getImage(course.data)}
             style={{ height: 450, width: 600, overflow: "hidden" }}
             alt={course.data?.title}
           />
