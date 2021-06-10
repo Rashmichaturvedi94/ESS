f773edb6046b56b1c402eceb62e3b029573627b4
sukhi0077 <sukhi0077@gmail.com>
2021-06-10 18:15:08 +0200

    Unique course subscription fix

--- backend/backend/apps/sub/models.py
+++ backend/backend/apps/sub/models.py
@@ -12,3 +12,7 @@ class SubModel(models.Model):
     active = models.BooleanField(default=False)
     def __str__(self):
         return self.task
+
+    class Meta:
+        managed = False
+        unique_together = (('course', 'subscriber'),)
