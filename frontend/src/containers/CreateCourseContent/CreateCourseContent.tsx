87f059d8ff84085631065a01b3302d2a99f40362
Manpreet Singh <mann.singh579@gmail.com>
2021-06-13 16:25:26 +0200

    redirect on course creation

--- frontend/src/containers/CreateCourseContent/CreateCourseContent.tsx
+++ frontend/src/containers/CreateCourseContent/CreateCourseContent.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import { CreateCourseContent as CreateCourseContentComponent } from './CreateCourseContent.styles';
 import { CreateCourseContentProps } from './CreateCourseContent.interface';
 import { CreateCourseContentForm } from '../../components/CreateCourseContentForm';
 import { usePostCourseContent } from '../../api';
 import { CourseParams } from '../../api/api.interface';
+import { appPaths } from '../../const/paths';
 
 export const CreateCourseContent: FC<CreateCourseContentProps> = () => {  
   const { mutate } = usePostCourseContent();
   const  { courseId } = useParams<CourseParams>();
-  
+  const history = useHistory();
+
   return (
     <CreateCourseContentComponent>
       <Box flex={1} display="flex" alignItems="center" justifyContent="center">
@@ -22,7 +24,7 @@ export const CreateCourseContent: FC<CreateCourseContentProps> = () => {
               onSubmit={(data) => {
                 const payload = data;
                 payload.course = (courseId ?? '1') as unknown as number;
-                mutate({ data: payload });
+                mutate({ data: payload }, { onSuccess: () => history.push(appPaths.searchCourse) });
               }}
             />
           </Box>
