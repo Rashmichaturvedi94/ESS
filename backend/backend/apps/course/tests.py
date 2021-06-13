8d2b61365e5dfcb0ba6b2cbeef1ff4f2d2186634
sukhi0077 <sukhi0077@gmail.com>
2021-06-13 15:41:36 +0200

    init

--- backend/backend/apps/course/tests.py
+++ backend/backend/apps/course/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+# from myproject.apps.core.models import Account
+
+# class AccountTests(APITestCase):
+#     def test_create_account(self):
+#         """
+#         Ensure we can get an Course.
+#         """
+#         url = reverse('course/1')
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(Account.objects.count(), 1)
+#         self.assertEqual(Account.objects.get().name, 'DabApps')
--- backend/backend/apps/sub/tests.py
+++ backend/backend/apps/sub/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+# from rest_framework.test import APIRequestFactory
+
+# # Using the standard RequestFactory API to create a form POST request
+# factory = APIRequestFactory()
+# request = factory.get('/subscri[tions/1')
